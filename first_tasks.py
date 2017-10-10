ec02e6c5ed7e1be0e61c9ec72c7f4aaa3c1bcea5
Adrián Casais <adrian.casais@rai.usc.es>
2017-10-10 14:18:31 +0200

    Actualizo

---
+++ first_tasks.py
@@ -0,0 +1,11 @@
+import numpy as np
+import ROOT as RT
+f = RT.Tfile('/scratch13/acasais/merged.root')
+OA
+# Adrian. Study velo (upstream) tracks.
+#  - Fraction of VELO tracks with respect to all tracks
+
+#  - Match velo tracks to long tracks. Fraction of times you get a DeltaR< 0.25?
+#  - Match velo tracks to mcparticles. Use smallest delta R.
+#  - Distribution of the IDs of the mcparticles matched to VELO tracks.
+#  - Momentum distribution of the mcparticles matched to VELO tracks. Same with eta and phi. Compare cases when close long tracks are present or not.
