b65ef864b40f18ddd8d34344d2db3d5b7c08c02e
Adrián Casais <adrian.casais@rai.usc.es>
2017-10-16 21:53:37 +0200

    Non me fai o histograma descartando as trazas duplicadas,actualizado

--- tasks/distributions/distributions.py
+++ tasks/distributions/distributions.py
@@ -21,18 +21,18 @@ for i in xrange(1,600):
 ##
 #momentum distribution of the particles matched to VELO
 
-# hP = TH1F('mom','',300,0,20000)
-# t.Project('mom','Particle_P','Track_type == 1')
-# c3 = TCanvas()
-# hP.Draw()
-# heta = TH1F('eta','',500,0,8)
-# t.Project('eta','Particle_eta','Track_type == 1')
-# c1 = TCanvas()
-# heta.Draw()
-# hphi = TH1F('phi','',20,-np.pi,np.pi)
-# t.Project('phi','Particle_phi','Track_type == 1')
-# c2 = TCanvas()
-# hphi.Draw()
+hP = TH1F('mom','',300,0,20000)
+t.Project('mom','Particle_P','Track_type == 1')
+c3 = TCanvas()
+hP.Draw()
+heta = TH1F('eta','',500,0,8)
+t.Project('eta','Particle_eta','Track_type == 1')
+c1 = TCanvas()
+heta.Draw()
+hphi = TH1F('phi','',20,-np.pi,np.pi)
+t.Project('phi','Particle_phi','Track_type == 1')
+c2 = TCanvas()
+hphi.Draw()
 
 # vou a quitar aqueles eventos q estean na tupla de MatchVeloLong
 f2 = TFile('/scratch13/acasais/MatchVeloLong/MatchVeloLong1-40000.root')
@@ -43,15 +43,15 @@ fhist =TFile('Histograma.root','recreate')
 event_no = 0
 #usar copytree!
 for evt in t:
-    if event_no == evt.Event_no: continue
-    else:     
+    if event_no != evt.Event_no:
         event_no = evt.Event_no
         tevent = t.CopyTree('Event_no=='+str(event_no))
         t2event = t2.CopyTree('Event_no=='+str(event_no))
     	for ev1 in tevent:
         	for ev2 in t2event:
             		cond2 = '((ev1.Track_px == ev2.Long_px and ev1.Track_py == ev2.Long_py) or (ev1.Track_px == ev2.Velo_px and ev1.Track_py == ev2.Velo_py)'
-            		if cond2:
+            		if not cond2:
+                                print event_no 
                 		hP2.Fill(ev1.Particle_P)
 			
 			
