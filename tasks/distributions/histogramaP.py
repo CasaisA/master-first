f93ac4d5e7f2b0fefcf86a3a092c784ae06c5ca4
Adrián Casais <adrian.casais@rai.usc.es>
2017-10-18 14:46:32 +0200

    Está facendo agora mesmo, veremos se fabrica o .root

--- tasks/distributions/histogramaP.py
+++ tasks/distributions/histogramaP.py
@@ -5,14 +5,10 @@ import numpy as np
 f = TFile('/scratch13/acasais/TrackMatching/TrackMatching1-30000.root')
 t = f.Get('aTree')
 
-
 #vou a quitar aqueles eventos q estean na tupla de MatchVeloLong          
-f2 = TFile('/scratch13/acasais/MatchVeloLong/MatchVeloLong1-40000.root')
-t2 = f2.Get('aTree')
-hP2=TH1F('momento w/o ghosts','',500,0,20000)
+f2 = TFile('/scratch13/acasais/MatchVeloLong/MatchVeloLong1-40000.root') t2 = f2.Get('aTree')
+hP2=TH1F('P w/o ghosts','',500,0,20000)
 fhist =TFile('Histograma.root','recreate')
-
-
 # esto vai moi lento                                                      
 event_no = 0
 events=[]
@@ -34,18 +30,14 @@ for evt in events:
         if t2event.GetEntries()==0 or tevent.GetEntries==0: continue
         for ev1 in tevent:
                 for ev2 in t2event:
-                    if (ev1.Track_px == ev2.Long_px and ev1.Track_py == e\
-v2.Long_py) or\
-                       (ev1.Track_px == ev2.Velo_px and ev1.Track_py == e\
-v2.Velo_px):
+                    if (ev1.Track_px == ev2.Long_px and ev1.Track_py == ev2.Long_py) or (ev1.Track_px == ev2.Velo_px and ev1.Track_py == ev2.Velo_px):
 
 
                         continue
                     else:
 			hP2.Fill(ev1.Particle_P)                          
                                                                           
-
 fhist.cd()
-hP2.Write()
+hP2.Write('P w/o ghosts')
 fhist.Close()
 
