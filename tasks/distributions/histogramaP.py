5a34dbf5c002502a6f1478483be247776857c550
Adrián Casais <adrian.casais@rai.usc.es>
2017-11-02 14:17:06 +0100

    Engadin mais arquivos

--- tasks/distributions/histogramaP.py
+++ tasks/distributions/histogramaP.py
@@ -6,13 +6,19 @@ f = TFile('/scratch13/acasais/TrackMatching/TrackMatching1-30000.root')
 t = f.Get('aTree')
 
 #vou a quitar aqueles eventos q estean na tupla de MatchVeloLong          
-f2 = TFile('/scratch13/acasais/MatchVeloLong/MatchVeloLong1-40000.root') t2 = f2.Get('aTree')
-hP2=TH1F('P w/o ghosts','',500,0,20000)
-fhist =TFile('Histograma.root','recreate')
+f2 = TFile('/scratch13/acasais/MatchVeloLong/MatchVeloLong1-40000.root')
+t2 = f2.Get('aTree')
+
+fhist =TFile('HwoGhosts-notree.root','recreate')
+hP=TH1F('p wo ghosts','',300,0,20000)
+heta = TH1F('eta wo ghosts','',500,0,8)
+hphi = TH1F('phi wo ghosts','',20,-np.pi,np.pi)
+
 # esto vai moi lento                                                      
 event_no = 0
 events=[]
 #usar copytree!                                                           
+
 for evt in t:
     if event_no != evt.Event_no:
         events.append(evt.Event_no)
@@ -20,24 +26,39 @@ for evt in t:
 t3 = t.CopyTree('Track_type==1')
 event_no = 0
 
-
+matches = 0 
 for evt in events:
     if event_no != evt:
+        tevent=0
+        t2event=0
         fera = TFile('eraseme.root','RECREATE')
         event_no = evt
         tevent = t3.CopyTree('Event_no=='+str(event_no))
         t2event = t2.CopyTree('Event_no=='+str(event_no))
         if t2event.GetEntries()==0 or tevent.GetEntries==0: continue
         for ev1 in tevent:
+                flag = 0
                 for ev2 in t2event:
-                    if (ev1.Track_px == ev2.Long_px and ev1.Track_py == ev2.Long_py) or (ev1.Track_px == ev2.Velo_px and ev1.Track_py == ev2.Velo_px):
-
-
-                        continue
-                    else:
-			hP2.Fill(ev1.Particle_P)                          
-                                                                          
+                    if (ev1.Track_px == ev2.Long_px and ev1.Track_py == ev2.Long_py and ev1.Track_pz == ev2.Long_pz) or (ev1.Track_px == ev2.Velo_px and ev1.Track_py == ev2.Velo_py and ev1.Track_pz == ev2.Velo_pz):
+                        matches +=1
+                        flag = 1
+                        if matches % 1000 == 0: print matches
+                        break
+                    
+                if flag == 0: 
+			hP.Fill(ev1.Particle_P)
+                        heta.Fill(ev1.Particle_eta)
+			hphi.Fill(ev1.Particle_phi)  
+#f.Close()
+#f2.Close()
+tevent = 0
+t2event = 0
+t = 0
+t2 = 0
+t3 = 0
 fhist.cd()
-hP2.Write('P w/o ghosts')
+hP.Write('P wo ghosts')
+heta.Write('eta wo ghosts')
+hphi.Write('phi wo ghosts')
 fhist.Close()
 
