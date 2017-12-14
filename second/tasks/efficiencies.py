75169910d48dead208c93059ef45bb1788fb1705
Adrián Casais <adrian.casais@rai.usc.es>
2017-12-14 13:41:06 +0100

    agora faino mellor

--- second/tasks/efficiencies.py
+++ second/tasks/efficiencies.py
@@ -1,3 +1,4 @@
+gROOT.ProcessLine(".x /cvmfs/lhcb.cern.ch/lib/lhcb/URANIA/URANIA_v1r1/RootTools/LHCbStyle/src/lhcbStyle.C")
 import numpy as np
 import math as m
 # import pandas as pd
@@ -26,9 +27,9 @@ fup = TFile('/scratch13/acasais/second/KsPiPiee-root/KsPiPiee_up_1-1000000.root'
 fdown = TFile('/scratch13/acasais/second/KsPiPiee-root/KsPiPiee_down_1-1000000.root')
 f = TFile('/scratch13/acasais/second/KsPiPiee-root/KsPiPiee.root')
 
-tks0t = f.Get('kS0_truth')
-tks0r_alltracks = f.Get('ks0products_reco')
-treco = f.Get('pi-e_reco')
+tks0t = fup.Get('kS0_truth')
+tks0r_alltracks = fup.Get('ks0products_reco')
+treco = fup.Get('pi-e_reco')
 ferase = TFile('eraseme.root','recreate')
 tks0r = tks0r_alltracks.CopyTree('(eminus_trck_type==1||eminus_trck_type==3)&&(eplus_trck_type==1||eplus_trck_type==3)&&(piminus_trck_type==1||piminus_trck_type==3)&&(piplus_trck_type==1||piplus_trck_type==3)')
 
@@ -109,8 +110,8 @@ mass = []
 #     mass.append(m.sqrt(E**2-p**2))
     
 
-f = TFile('eraseme.root','recreate')
-tlong3=tks0r.CopyTree(long3+'&&!'+pack(long4))
+f1 = TFile('eraseme.root','recreate')
+tlong3=tks0r.CopyTree(long3)
 for evt in tlong3:
         ux = evt.PV_x-evt.SV_x
         uy = evt.PV_y-evt.SV_y
@@ -119,17 +120,22 @@ for evt in tlong3:
         ux= ux/u
         uy = uy/u
         uz = uz/u
-        sinthetapiplus = abs(m.sqrt(1-((evt.piplus_px*ux+evt.piplus_py*uy+evt.piplus_pz*uz)/evt.piplus_p)**2))
-        sinthetapiminus = abs(m.sqrt(1-((evt.piminus_px*ux+evt.piminus_py*uy+evt.piminus_pz*uz)/evt.piminus_p)**2))
-        sinthetaeminus = abs(m.sqrt(1-((evt.eminus_px*ux+evt.eminus_py*uy+evt.eminus_pz*uz)/evt.eminus_p)**2))
-        sinthetaeplus = abs(m.sqrt(1-((evt.eplus_px*ux+evt.eplus_py*uy+evt.eplus_pz*uz)/evt.eplus_p)**2))
-        ptpiplus = evt.piplus_p*sinthetapiplus
-        ptpiminus = evt.piminus_p*sinthetapiminus
-        
+        #uprim e uprima, o vector resultante dos outros tres, que ten q ser coplanario co momento do electron/positron
+        uprim_x = evt.piplus_px+evt.piminus_px
+        uprim_y = evt.piplus_py+evt.piminus_py
+        uprim_z = evt.piplus_pz+evt.piminus_pz
         if evt.eplus_trck_type == 1:
-            pteminus = evt.eminus_p*sinthetaeminus
-            pteplus = -pteminus-ptpiminus-ptpiplus
-            pe=abs(pteplus/sinthetaeplus)
+            uprim_x+=evt.eminus_px
+            uprim_y+=evt.eminus_py
+            uprim_z+=evt.eminus_pz
+            uprim = m.sqrt(uprim_x**2+uprim_y**2+uprim_z**2)
+            costhetaeplus = (ux*evt.eplus_px + uy*evt.eplus_py + uz*evt.eplus_pz)/evt.eplus_p
+            sinthetaeplus = m.sqrt(1-costhetaeplus**2)
+            costhetauprim= (ux*uprim_x + uy*uprim_y + uz*uprim_z)/uprim
+            sinthetauprim = m.sqrt(1-costhetauprim**2)
+
+            pe=uprim*sinthetauprim/sinthetaeplus
+            
             eplus_px = pe*evt.eplus_px/evt.eplus_p
             eplus_py = pe*evt.eplus_py/evt.eplus_p
             eplus_pz = pe*evt.eplus_pz/evt.eplus_p
@@ -139,9 +145,18 @@ for evt in tlong3:
             eminus_pz = evt.eminus_pz
         
         if evt.eminus_trck_type == 1:
-            pteplus = evt.eplus_p*sinthetaeplus
-            pteminus = -pteplus-ptpiminus-ptpiplus
-            pe=abs(pteminus/sinthetaeminus)
+            uprim_x+=evt.eplus_px
+            uprim_y+=evt.eplus_py
+            uprim_z+=evt.eplus_pz
+            uprim = m.sqrt(uprim_x**2+uprim_y**2+uprim_z**2)
+            costhetaeminus = (ux*evt.eminus_px + uy*evt.eminus_py + uz*evt.eminus_pz)/evt.eminus_p
+            sinthetaeminus = m.sqrt(1-costhetaeminus**2)
+            costhetauprim= (ux*uprim_x + uy*uprim_y + uz*uprim_z)/uprim
+            sinthetauprim = m.sqrt(1-costhetauprim**2)
+
+            pe=uprim*sinthetauprim/sinthetaeminus
+
+            
             eminus_px = pe*evt.eminus_px/evt.eminus_p
             eminus_py = pe*evt.eminus_py/evt.eminus_p
             eminus_pz = pe*evt.eminus_pz/evt.eminus_p
@@ -168,6 +183,18 @@ for evt in tlong3:
         p = m.sqrt(px**2+py**2+pz**2)
         E = m.sqrt(eminus_p**2+0.511**2)+m.sqrt(eplus_p**2+0.511**2)+m.sqrt(evt.piminus_p**2+139.57018**2)+m.sqrt(evt.piplus_p**2+139.57018**2)
         mass.append(m.sqrt(E**2-p**2))
-import matplotlib.pyplot as plt
-hist = plt.hist(mass,bins=50)
-plt.show()
+# import matplotlib.pyplot as plt
+# hist = plt.hist(mass,bins=50)
+# plt.show()
+
+
+h = TH1F('masa ks0','masa ks0',50,400.,1400.0)
+
+for mas in mass:
+    h.Fill(mas)
+
+h.SetXTitle('M(K_{S}^{0})[MeV/c^{2}]')
+h.SetYTitle('No.Entries / 20 MeV/c^{2}')
+h.Draw()
+mean = h.GetMean()
+std_dev = h.GetStdDev()
